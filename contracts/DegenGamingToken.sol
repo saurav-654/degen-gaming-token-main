f9a9c8b6b33c9d36887b62d306554371f9f12f6b
Saurav Agrawal <128025275+saurav-654@users.noreply.github.com>
2024-07-20 14:34:28 +0530

    Update DegenGamingToken.sol

--- contracts/DegenGamingToken.sol
+++ contracts/DegenGamingToken.sol
@@ -1,69 +1,66 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-contract DegenToken is ERC20, Ownable{
-    constructor() ERC20("Degen Gaming Token", "DGT") {}
-    address ownerOfContract = msg.sender;
+contract DegenToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("Degen Gaming Token", "DGT") Ownable(initialOwner) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function burnTokens(uint amount) public {
+    function burnTokens(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-    function decimals() override public pure returns (uint8) {
-        return 0;//100 tokens will be equal to 100 tokens only
+    function decimals() public pure override returns (uint8) {
+        return 0;
     }
 
     function getBalance() external view returns (uint256) {
-        return this.balanceOf(msg.sender);
+        return balanceOf(msg.sender);
     }
 
-    string public storeItems = "The following items are available to be redeemed for tokens; 1 Official degen NFT(100 DGN) 2 The Hammer of Ragnarok(200 DGN) 3 The sword of god(300 DGN) 4 Dragon's breath(400 DGN)";
+    string public constant STORE_ITEMS = "The following items are available to be redeemed for tokens: 1. Gaming tokens NFT (100 DGT) 2. Alpha (200 DGT) 3. Beta (300 DGT) 4. Gama (400 DGT)";
 
-    function redeemTokens(uint8 num) external returns(string memory){
-        if(num == 1){
-            require(balanceOf(msg.sender) >= 100, "You do not have enough Degen Tokens");
-            bool success = transfer(ownerOfContract, 100);
-            if(success == true){
-                return "You are now the proud owner of Official degen NFT!";
-            }
-        }
-        else if(num == 2){
-            require(balanceOf(msg.sender) >= 200, "You do not have enough Degen Tokens");
-            bool success = transfer(ownerOfContract, 200);
-            if(success == true){
-                return "You are now the proud owner of The Hammer of Ragnarok!";
-            }
-        }
-        else if(num == 3){
-            require(balanceOf(msg.sender) >= 300, "You do not have enough Degen Tokens");
-            bool success = transfer(ownerOfContract, 300);
-            if(success == true){
-                return "You are now the proud owner of The sword of god!";
-            }
-        }
-        else if(num == 4){
-            require(balanceOf(msg.sender) >= 400, "You do not have enough Degen Tokens");
-            bool success = transfer(ownerOfContract, 400);
-            if(success == true){
-                return "You are now the proud owner of Dragon's breath!";
-            }
-        }
-        
-        return "Invalid input";
-    }
+    function redeemTokens(uint8 num) external returns (string memory) {
+        uint256 cost;
+        string memory item;
 
-    function transfer(address recipient, uint amount) override public returns(bool){
-        require(balanceOf(msg.sender) >= amount, "You do not have enough Degen Tokens");
-        _transfer(msg.sender, recipient, amount);
-        emit Transfer(msg.sender, recipient, amount);
-        return true;//function to this call was successful
+        if (num == 1) {
+            cost = 100;
+            item = "Gaming tokens NFT";
+        } else if (num == 2) {
+            cost = 200;
+            item = "Alpha";
+        } else if (num == 3) {
+            cost = 300;
+            item = "Beta";
+        } else if (num == 4) {
+            cost = 400;
+            item = "Gama";
+        } else {
+            revert("Invalid item number");
+        }
+
+        uint256 callerBalance = balanceOf(msg.sender);
+
+        if (callerBalance < cost) {
+            uint256 neededTokens = cost - callerBalance;
+            // Optionally mint tokens to the caller if the caller is the owner
+            if (msg.sender == owner()) {
+                mint(msg.sender, neededTokens);
+            } else {
+                revert("Insufficient Degen Tokens and not the owner to mint");
+            }
+        }
+
+        require(balanceOf(msg.sender) >= cost, "Insufficient Degen Tokens");
+        _transfer(msg.sender, owner(), cost);
+        return string(abi.encodePacked("You are now the proud owner of ", item, "!"));
     }
 }
-
