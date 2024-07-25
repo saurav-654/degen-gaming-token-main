76e875217b98f4b6196d3131612cface4a7e7ce6
Saurav Agrawal <128025275+saurav-654@users.noreply.github.com>
2024-07-25 08:48:09 +0530

    DegenGamingToken.sol

--- contracts/DegenGamingToken.sol
+++ contracts/DegenGamingToken.sol
@@ -3,18 +3,26 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "hardhat/console.sol";
 
 contract DegenToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("Degen Gaming Token", "DGT") Ownable(initialOwner) {}
+    constructor() ERC20("Degen Gaming Token", "DGT") Ownable(msg.sender) {
+        
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
     function burnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance to burn");
         _burn(msg.sender, amount);
+    }
+
+    function transferTokens(address to, uint256 amount) public returns (bool) {
+        require(to != address(0), "Invalid recipient address");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance to transfer");
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 
     function decimals() public pure override returns (uint8) {
@@ -25,11 +33,14 @@ contract DegenToken is ERC20, Ownable {
         return balanceOf(msg.sender);
     }
 
-    string public constant STORE_ITEMS = "The following items are available to be redeemed for tokens: 1. Gaming tokens NFT (100 DGT) 2. Alpha (200 DGT) 3. Beta (300 DGT) 4. Gama (400 DGT)";
+    string public constant STORE_ITEMS = "The following items are available to be redeemed for tokens: 1. Gaming tokens NFT (100 DGT) 2. Alpha (200 DGT) 3. Gamma (300 DGT) 4. Beta (400 DGT)";
 
-    function redeemTokens(uint8 num) external returns (string memory) {
+    
+     event TokensRedeemed(address indexed user, uint8 itemNumber, uint256 cost, string item, string message);
+     function redeemTokens(uint8 num) external  {
         uint256 cost;
         string memory item;
+        string memory successMsg = "You have successfully redeemed";
 
         if (num == 1) {
             cost = 100;
@@ -39,28 +50,21 @@ contract DegenToken is ERC20, Ownable {
             item = "Alpha";
         } else if (num == 3) {
             cost = 300;
-            item = "Beta";
+            item = "Gamma";
         } else if (num == 4) {
             cost = 400;
-            item = "Gama";
+            item = "Beta";
         } else {
             revert("Invalid item number");
         }
 
-        uint256 callerBalance = balanceOf(msg.sender);
-
-        if (callerBalance < cost) {
-            uint256 neededTokens = cost - callerBalance;
-            // Optionally mint tokens to the caller if the caller is the owner
-            if (msg.sender == owner()) {
-                mint(msg.sender, neededTokens);
-            } else {
-                revert("Insufficient Degen Tokens and not the owner to mint");
-            }
-        }
-
         require(balanceOf(msg.sender) >= cost, "Insufficient Degen Tokens");
-        _transfer(msg.sender, owner(), cost);
-        return string(abi.encodePacked("You are now the proud owner of ", item, "!"));
+        _burn(msg.sender, cost);
+        
+        // Emit an event for off-chain tracking of redemptions
+        emit TokensRedeemed(msg.sender, num, cost, item, string(abi.encodePacked(successMsg, " ", item)));
     }
+
+    
+    
 }
